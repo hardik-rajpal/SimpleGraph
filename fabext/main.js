f24723d55f1bfb634364c5ad361f9694c1449dc3
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-10-24 20:51:03 +0530

    data.json loaded into html. The JSON has landed!

--- fabext/main.js
+++ fabext/main.js
@@ -7,34 +7,19 @@ let mainWindow;
 
 //listen for the app to be ready.
 app.on('ready', function(){
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        width:300,
+        height:200,
+        title:'Add File',
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation:false
+        }
+    });
     mainWindow.loadURL(url.format({
         pathname:path.join(__dirname, 'mainWindow.html'),
         protocol:'file:',
         slashes:true
     }))
-    //Build menu from tempalte
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);
-})
 
-const mainMenuTemplate = [
-    {
-        label:'File',
-        submenu:[
-            {
-                label:'Add Item',
-                click(){
-                    createAddWindow();
-                }
-            },
-            {
-                label:'Quit',//shortcuts:
-                accelerator:  process.platform=='darwin' ? 'Command+Q':'Ctrl+Q', 
-                click(){
-                    app.quit();
-                }
-            }
-        ]
-    }
-]
+})
